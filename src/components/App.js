5f31f0160d78d43ac5cad241ac8dc41893c1415e
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-04-12 19:26:54 -0600

    Add react-router and links in nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './Home'
+import About from './About'
+import Topic from './Topic'
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div>Hello world</div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+          <hr/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/topics' component={Topic}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
 
-export default function Topics () {
+export default function Topic () {
     return <div>Topics</div>
 }
