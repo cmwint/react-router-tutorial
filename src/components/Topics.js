1ca56e4957659678fc91388eb544ae8d3d4d470e
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-04-12 20:05:24 -0600

    Match url with dyanmic params

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link, Route } from 'react-router-dom'
 
 function Topic({ match }) {
+  console.log(match)
   return <h3>{match.params.topicId}</h3>
 }
 
-export default function Topics () {
+export default function Topics ({ match }) {
   return (
     <div>
         <h2>Topics</h2>
         <ul>
             <li>
-              <Link to='/topics/rendering-with-react'>Rendering with React</Link>
+              <Link to={`${match.url}/rendering-with-react`}>Rendering with React</Link>
             </li>
             <li>
-              <Link to='/topics/components'>Components</Link>
+              <Link to={`${match.url}/components`}>Components</Link>
             </li>
             <li>
-              <Link to='/topics/props-v-state'>Props vs State</Link>
+              <Link to={`${match.url}/props-v-state`}>Props vs State</Link>
             </li>
         </ul>
         <hr/>
-        <Route path='/topics/:topicId' component={Topic} />
+        <Route path={`${match.path}/:topicId`} component={Topic} />
     </div>
   )
 }
